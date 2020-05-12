c1608bbfc13eb4741ac2e5e71e6724f561b50f2d
JeremieGKlee <jeremie.gosgnack@gmail.com>
2020-05-12 17:53:15 +0200

    feat: frontend

---
+++ src/Controller/PropertyController.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Property;
+use App\Repository\PropertyRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class PropertyController extends AbstractController
+{
+    /**
+     * @var PropertyRepository
+     */
+    private $repository;
+    
+    public function __construct(PropertyRepository $repository, EntityManagerInterface $em)
+    {
+        $this->repository = $repository;
+        $this->em = $em;
+    }
+    
+    /**
+     * @Route("/property", name="property.index")
+     * @return Response
+     */
+    public function index():Response
+    {
+        // $property = $this->repository->findAllVisible();
+        // $property['0']->setSold(true);
+        // $this->em->flush();
+        // dump($property);
+        return $this->render('property/index.html.twig', [
+            'current_menu' => 'properties',
+        ]);
+    }
+
+    /**
+     * @Route("/property/{slug}-{id}", name="property.show", requirements={"slug": "[a-z0-9\-]*"})
+     * @param Property $property
+     * @return Response
+     */
+    // public function show($slug, $id): Response
+    public function show(Property $property, string $slug): Response
+    {
+        // $property = $this->repository->find($id);  avec premiere function  public function show($slug, $id): Response
+        if ($property->getSlug() !== $slug)
+        {
+            return $this-> redirectToRoute('property.show',
+            [
+                'id'=> $property->getId(),
+                'slug' => $property->getSlug()
+            ],301);
+        }
+        return $this->render('property/show.html.twig', [
+            'property' => $property,
+            'current_menu' => 'properties',
+        ]);
+    }
+}
